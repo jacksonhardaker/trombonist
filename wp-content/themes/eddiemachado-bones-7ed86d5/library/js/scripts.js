6050bd0a4f9fbbd52e6c275c39bffb2d7df734e3
Jackson Hardaker <jackson.hardaker@gmail.com>
2022-07-14 07:02:14 -0500

    replace with statically generated archive

---
+++ wp-content/themes/eddiemachado-bones-7ed86d5/library/js/scripts.js
@@ -0,0 +1,163 @@
+/*
+Bones Scripts File
+Author: Eddie Machado
+
+This file should contain any js scripts you want to add to the site.
+Instead of calling it in the header or throwing it inside wp_head()
+this file will be called automatically in the footer so as not to
+slow the page load.
+
+*/
+
+// IE8 ployfill for GetComputed Style (for Responsive Script below)
+if (!window.getComputedStyle) {
+	window.getComputedStyle = function(el, pseudo) {
+		this.el = el;
+		this.getPropertyValue = function(prop) {
+			var re = /(\-([a-z]){1})/g;
+			if (prop == 'float') prop = 'styleFloat';
+			if (re.test(prop)) {
+				prop = prop.replace(re, function () {
+					return arguments[2].toUpperCase();
+				});
+			}
+			return el.currentStyle[prop] ? el.currentStyle[prop] : null;
+		}
+		return this;
+	}
+}
+
+// as the page loads, call these scripts
+jQuery(document).ready(function($) {
+
+	/*
+	Responsive jQuery is a tricky thing.
+	There's a bunch of different ways to handle
+	it, so be sure to research and find the one
+	that works for you best.
+	*/
+	
+	/* getting viewport width */
+	var responsive_viewport = $(window).width();
+	
+	/* if is below 481px */
+	if (responsive_viewport < 481) {
+	
+	} /* end smallest screen */
+	
+	/* if is larger than 481px */
+	if (responsive_viewport > 481) {
+	
+	} /* end larger than 481px */
+	
+	/* if is above or equal to 768px */
+	if (responsive_viewport >= 768) {
+	
+		/* load gravatars */
+		$('.comment img[data-gravatar]').each(function(){
+			$(this).attr('src',$(this).attr('data-gravatar'));
+		});
+		
+	}
+	
+	/* off the bat large screen actions */
+	if (responsive_viewport > 1030) {
+	
+	}
+	
+	
+	// add all your scripts here
+	
+ 
+}); /* end of as page load scripts */
+
+
+/*! A fix for the iOS orientationchange zoom bug.
+ Script by @scottjehl, rebound by @wilto.
+ MIT License.
+*/
+(function(w){
+	// This fix addresses an iOS bug, so return early if the UA claims it's something else.
+	if( !( /iPhone|iPad|iPod/.test( navigator.platform ) && navigator.userAgent.indexOf( "AppleWebKit" ) > -1 ) ){ return; }
+	var doc = w.document;
+	if( !doc.querySelector ){ return; }
+	var meta = doc.querySelector( "meta[name=viewport]" ),
+		initialContent = meta && meta.getAttribute( "content" ),
+		disabledZoom = initialContent + ",maximum-scale=1",
+		enabledZoom = initialContent + ",maximum-scale=10",
+		enabled = true,
+		x, y, z, aig;
+	if( !meta ){ return; }
+	function restoreZoom(){
+		meta.setAttribute( "content", enabledZoom );
+		enabled = true; }
+	function disableZoom(){
+		meta.setAttribute( "content", disabledZoom );
+		enabled = false; }
+	function checkTilt( e ){
+		aig = e.accelerationIncludingGravity;
+		x = Math.abs( aig.x );
+		y = Math.abs( aig.y );
+		z = Math.abs( aig.z );
+		// If portrait orientation and in one of the danger zones
+		if( !w.orientation && ( x > 7 || ( ( z > 6 && y < 8 || z < 8 && y > 6 ) && x > 5 ) ) ){
+			if( enabled ){ disableZoom(); } }
+		else if( !enabled ){ restoreZoom(); } }
+	w.addEventListener( "orientationchange", restoreZoom, false );
+	w.addEventListener( "devicemotion", checkTilt, false );
+})( this );
+
+/* jQuery.browser.mobile (http://detectmobilebrowser.com/)
+ *
+ * jQuery.browser.mobile will be true if the browser is a mobile device
+ *
+ **/
+(function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
+
+if (!$.browser.mobile && $("body").width() > 768)
+{
+  var _fixedHeader = false;
+  var _fixedHeaderThreshold = null;
+  var _header = $($("nav")[0]);
+  
+  $(document).ready(function(){
+    _fixedHeaderThreshold = _header.offset().top;
+    $($("#mobile-menu").children()[0]).click(function(){$($("nav")[0]).toggleClass("show"); });
+    $(window).scroll(function(){
+      var top = $(window).scrollTop();
+      if (top >= _fixedHeaderThreshold && !_fixedHeader)
+      {
+        _fixedHeader = true;
+        _header.addClass("fixed");
+        $(".top-nav").addClass("wrap");
+        $("#stationary-menu").addClass("wrap transition boxShadowDefault");
+        $($("header")[0]).addClass("fixedmenu");
+        $("#flyin-header").addClass("transition");
+        setTimeout(function() {
+          $(".top-nav").addClass("");
+          $("#stationary-menu").addClass("fullwidth");
+          setTimeout(function() {$("#flyin-header").addClass("show");}, 30);
+      }, 10);
+      }
+      else if (top < _fixedHeaderThreshold && _fixedHeader)
+      {
+        _fixedHeader = false;
+        $("#flyin-header").removeClass("transition");
+        //$(".top-nav").removeClass("");
+        $("#stationary-menu").removeClass("fullwidth");
+        $("#flyin-header").removeClass("show");
+        _header.removeClass("fixed");
+        $($("header")[0]).removeClass("fixedmenu");
+        $(".top-nav").removeClass("wrap");
+        $("#stationary-menu").removeClass("wrap transition boxShadowDefault");
+      }
+    });
+  });
+}
+else
+{
+  $(document).ready(function(){
+    $($("nav")[0]).addClass("transition-v-slow");
+    $($("#mobile-menu").children()[0]).click(function(){$($("nav")[0]).toggleClass("show"); });
+  });
+}
